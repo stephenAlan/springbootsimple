79db733fb737e71dbba6bfa0d91b16109447dba7
shouchao.shi <stephenAlan@163.com>
2021-01-27 14:21:37 +0800

    统一参数校验拦截器

--- src/main/java/com/stephen/simple/controller/HelloController.java
+++ src/main/java/com/stephen/simple/controller/HelloController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
@@ -15,7 +17,11 @@ import java.net.UnknownHostException;
  * Description:
  */
 @RestController
+// @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class HelloController {
+
+    // @Resource
+    // private AppConfig appConfig;
 
     @Bean
     public String str1(){
@@ -25,17 +31,26 @@ public class HelloController {
     @RequestMapping()
     public Object hello() throws UnknownHostException {
         String ipv4 = Inet4Address.getLocalHost().getHostAddress();
+        // System.out.println(appConfig.getUrl());
+        // System.out.println(appConfig.getUsername());
         return ipv4 + " hello dockerfile hello world";
     }
 
     @RequestMapping("/hello")
-    public Object hello1(@RequestParam String name) {
-        return name + " hello dockerfile hello world " ;
+    public Object hello1(@RequestParam String name, HttpServletRequest request/*,@RequestBody User user*/) {
+        int age = Integer.valueOf(request.getParameter("age"));
+        // return user.getName() + " hello dockerfile hello world " + user.getAge() + "\t" + age + "\t" + name;
+        return name + " hello dockerfile hello world " + age ;
     }
 
     @RequestMapping("/body")
-    public Object body(@RequestBody User user) {
-        return user.getName() + "\t" + user.getAge() + " hello dockerfile hello world " ;
+    public Object body(@Valid @RequestBody User user) {
+        try {
+            return user.getName() + "\t" + user.getAge() + " hello dockerfile hello world " ;
+        } catch (Exception e) {
+
+        }
+        return null;
     }
 
 }
--- src/main/java/com/stephen/simple/entity/User.java
+++ src/main/java/com/stephen/simple/entity/User.java
@@ -2,6 +2,7 @@ package com.stephen.simple.entity;
 
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * Created by ssc on 2021-01-11 9:59 .
@@ -11,6 +12,7 @@ import lombok.Data;
 @Builder
 public class User {
 
+    @Length(min = 5)
     private String name;
     private Integer age;
 
